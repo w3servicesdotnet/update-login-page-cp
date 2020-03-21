aca02b3059ff310a2c302ffb305812692f727331
W3SERVICES <32176465+w3servicesdotnet@users.noreply.github.com>
2020-03-21 18:37:32 +0530

    Update update_one_click.sh

--- update_one_click.sh
+++ update_one_click.sh
@@ -4,10 +4,10 @@ UPDATE_DIR=/W3SERVICES_SCRIPT/UPDATEME
 
 mkdir -p $UPDATE_DIR/log
 cd $UPDATE_DIR
-wget https://raw.githubusercontent.com/aniket2797/update/master/update_one_click.sh
-wget https://raw.githubusercontent.com/aniket2797/update/master/page_update.sh
-wget https://raw.githubusercontent.com/aniket2797/update/master/default_html.html
-wget https://raw.githubusercontent.com/aniket2797/update/master/login.html
+wget https://raw.githubusercontent.com/w3servicesdotnet/update-login-page-cp/master/update_one_click.sh
+wget https://raw.githubusercontent.com/w3servicesdotnet/update-login-page-cp/master/page_update.sh
+wget https://raw.githubusercontent.com/w3servicesdotnet/update-login-page-cp/master/default_html.html
+wget https://raw.githubusercontent.com/w3servicesdotnet/update-login-page-cp/master/login.html
 
 chmod +x page_update.sh
 
